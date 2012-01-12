2017d3a7d05559296a6466ee12cde77e1ea1e983
Daniel Carneiro <daniel.carneiro@biodevice.pt>
2012-01-12 16:02:33 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '43f9914d21beb13d85188bec37b6f9e3b36404d186773592031ee759791adc0452fd02300ff7550f00bc5ff1b8d963d149cc95c0d2ddf04b115f780a126590bb'
